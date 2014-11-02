2e9f3635d96482e3abb06b1ebe892181a7f34fdc
ruohan.chen <crhan123@gmail.com>
2014-11-02 15:46:10 +0800

    add some indent

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,17 +1,17 @@
 module Calc
   def self.eval(string)
     result = case string
-    when /-/
-      string.split('-').map(&:to_i).reduce(&:-)
-    when /\+/
-      string.split('+').map(&:to_i).reduce(&:+)
-    when /\*/
-      string.split('*').map(&:to_i).reduce(&:*)
-    when /\//
-      string.split('/').map(&:to_i).reduce(&:/)
-    else
-      string.to_i
-    end
+             when /-/
+               string.split('-').map(&:to_i).reduce(&:-)
+             when /\+/
+               string.split('+').map(&:to_i).reduce(&:+)
+             when /\*/
+               string.split('*').map(&:to_i).reduce(&:*)
+             when /\//
+               string.split('/').map(&:to_i).reduce(&:/)
+             else
+               string.to_i
+             end
     [result, 0].max
   end
 end
